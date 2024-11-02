a42619bf869d902bffce06ed18a80d822a5b257e
MrMinemeet <alex@wtf-my-code.works>
2024-11-02 11:01:46 +0100

    Add initial implementation of AmznShipWarn extension
    - Create README.md with project description and functionality
    - Implement amznshipwarn.js to check for non-Amazon shipping offers and display warnings
    - Add manifest.json for extension configuration and content script setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AmznShipWarn
 A browser extension that adds a warning to the Amazon page, if the item is not shipped by Amazon directly
+
+## How does it work?
+The exension checks the page for the text that holds the shipping information. If the text contains the word "Amazon", the extension does nothing. If the text does not contain the word "Amazon", the extension adds a warning to the page. In order to get the user's attention and make them aware of potential shipping costs.
---
+++ amznshipwarn.js
@@ -0,0 +1,57 @@
+/**
+ * This extension is created by Alexander Voglsperger (MrMinemeet) and is licensed under the MIT License.
+ * See LICENSE.md for more information.
+ */
+console.debug("Amazon Shipping Warning Extension loaded");
+
+// Get element of class "offer-display-feature" and "offer-display-feature-name=*fulfiller-info"
+const offerTextElements = document.getElementsByClassName("offer-display-feature-text a-spacing-none");
+const offerTextElementsArray = Array.from(offerTextElements);
+
+// Check if the inner-html for any non-amazon "Sending" offers
+const shippmentEntries = offerTextElementsArray
+	.filter(element => !element.innerText.includes("Amazon") && !element.innerHTML.includes("<a href"))
+
+if (shippmentEntries.length > 0) {
+	console.info("Non-Amazon offers found", shippmentEntries);
+	// Display warning
+	const warningElement = document.createElement("div");
+	warningElement.style = "background-color: #ff0000; color: #ffffff; padding: 5px; margin: 5px; border-radius: 5px;";
+	warningElement.innerText = `⚠️ ${getWarning()}`;
+	warningElement.title = `⚠️ ${getWarning()}`;
+
+	// Insert warning in seller/shipping info
+	const firstElement = shippmentEntries[0];
+	firstElement.parentNode.insertBefore(warningElement, firstElement);
+
+	// Add warning symbols to "Buy now" and "Add to cart" buttons
+	const buyNowButton = document.getElementById("submit.buy-now-announce");
+	const addToCartButton = document.getElementById("submit.add-to-cart-announce");
+	if (addToCartButton) {
+		console.debug(addToCartButton)
+		addToCartButton.textContent = `📦🚨 ${addToCartButton.textContent}`;
+	}
+	if (buyNowButton) {
+		buyNowButton.textContent = `📦🚨 ${buyNowButton.textContent}`;
+	}
+}
+
+/**
+ * Returns the warning message based on the current hostname
+ * @returns {string} Warning message
+ */
+function getWarning() {
+	switch (window.location.hostname.replace("www.", "").replace("amazon.", "")) {
+		case "de":
+			return "Dieses Produkt wird nicht von Amazon versendet";
+		case "fr":
+			return "Ce produit n'est pas expédié par Amazon";
+		case "it":
+			return "Questo prodotto non è spedito da Amazon";
+		case "es", "com.mx":
+			return "Este producto no es enviado por Amazon";
+		// TODO: Add more countries based on the Amazon TLD
+		default:
+			return "This product is not shipped by Amazon";
+	}
+}
