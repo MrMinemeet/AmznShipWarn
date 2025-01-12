09fc622497c4743e3838d774f5e10a13a4c864c5
Alexander Voglsperger <alex@wtf-my-code.works>
2025-01-12 21:51:07 +0100

    Enhance js to utlize basic ts-checks (#3)
    Also reduce "getWarning()" call to just once.
    Furthermore, change way of setting the style

--- amznshipwarn.js
+++ amznshipwarn.js
@@ -1,3 +1,4 @@
+// @ts-check
 /**
  * This extension is created by Alexander Voglsperger (MrMinemeet) and is licensed under the MIT License.
  * See LICENSE file for more information or visit https://github.com/MrMinemeet/AmznShipWarn/blob/main/LICENSE
@@ -10,28 +11,34 @@ const offerTextElementsArray = Array.from(offerTextElements);
 
 // Check if the inner-html for any non-amazon "Sending" offers
 const shippmentEntries = offerTextElementsArray
-	.filter(element => !element.innerText.includes("Amazon") && !element.innerHTML.includes("<a href"))
+	.filter(element => !element.innerText.includes("Amazon") && !element.innerHTML.includes("<a href"))[0];
 
-if (shippmentEntries.length > 0) {
+if (shippmentEntries != null) {
 	console.info("Non-Amazon offers found! Adding warning to page");
 	// Display warning
 	const warningElement = document.createElement("div");
-	warningElement.style = "background-color: #ff0000; color: #ffffff; padding: 5px; margin: 5px; border-radius: 5px;";
-	warningElement.innerText = `⚠️ ${getWarning()}`;
-	warningElement.title = `⚠️ ${getWarning()}`;
+	warningElement.style.backgroundColor = "#ff0000";
+	warningElement.style.color = "#ffffff";
+	warningElement.style.padding = "5px";
+	warningElement.style.margin = "5px";
+	warningElement.style.borderRadius = "5px";
+	
+	const warningText = `⚠️ ${getWarning()}`;
+	warningElement.innerText = warningText;
+	warningElement.title = warningText;
 
 	// Insert warning in seller/shipping info
 	const firstElement = shippmentEntries[0];
 	firstElement.parentNode.insertBefore(warningElement, firstElement);
 
 	// Add warning symbols to "Buy now" and "Add to cart" buttons
-	const buyNowButton = document.getElementById("submit.buy-now-announce");
 	const addToCartButton = document.getElementById("submit.add-to-cart-announce");
 	if (addToCartButton) {
-		addToCartButton.textContent = `📦🚨 ${addToCartButton.textContent}`;
+		addToCartButton.textContent = `📦🚨 ${addToCartButton.textContent}`;;
 	}
+	const buyNowButton = document.getElementById("submit.buy-now-announce");
 	if (buyNowButton) {
-		buyNowButton.textContent = `📦🚨 ${buyNowButton.textContent}`;
+		buyNowButton.textContent = `📦🚨 ${buyNowButton.textContent}`;;
 	}
 }
 
